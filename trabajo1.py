a89b3992d7663433ac6f71371e29aade2b81f255
emily396 <emilyguerragalvan@gmail.com>
2025-03-21 15:55:48 -0600

    Add files via upload
    Trabajos implementa

---
+++ trabajo1.py
@@ -0,0 +1,12 @@
+z = 8
+if z > 8:
+    print("¡No voy a imprimir!") #ests secuencis no se ejecuta
+elif z > 5:
+ 	print("¡yo lo hare!") #esta sentencia se ejecuta 
+elif z > 6:
+ 	print("¡tampoco voy a imprimir!") #esta sentencia no se ejecuta
+else:
+ 	pritnt("!yo tampoco!") #esta sentencia no se ejecutara
+
+
+	
